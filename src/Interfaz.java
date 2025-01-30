45cbd5b0466762e23e3b446eb4da40954e0994c8
Aitor de Santos Amoros <soyadsa06@gmail.com>
2025-01-30 22:06:44 +0100

    Implementacion de la clase player para modularizar el proyecto

--- src/Interfaz.java
+++ src/Interfaz.java
@@ -1,6 +1,7 @@
 import java.util.HashSet;
 import java.util.Set;
 
+import Clases.Player;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
@@ -17,13 +18,7 @@ import javafx.scene.input.KeyCode;
 
 public class Interfaz extends Application {
 
-    // TOODO ESTE BLOQUE ES PARA LA ANIMACION DEL PLAYER
-    private int frameIndex = 0; // Índice del fotograma actual
-    private int SheetIndex = 0; // Índice del Sheet actual
-    private final int SheetCount = 4; // Cantidad de Sheets
-    private final int frameCount = 4; // Cantidad de fotogramas en la animación
-    private final int frameWidth = 16*4; // Ancho de cada fotograma en píxeles
-    private final int frameHeight = 32*4; // Alto del fotograma
+    private int velocidad = 5;
 
     private final Set<String> keysPressed = new HashSet<>();
 
@@ -36,33 +31,20 @@ public class Interfaz extends Application {
     @Override
     public void start(Stage stage) {
         Pane gamePane = new Pane();
-
+        Pane backgraundPane = new Pane();
+        
         // Creo una imagen con los siguientes parametros: Ruta, ancho, alto, preserveRatio, smooth (Aunque piendo que esta bug las 2 ultimas)
         // https://openjfx.io/javadoc/21/javafx.graphics/javafx/scene/image/Image.html
         // https://openjfx.io/javadoc/21/javafx.graphics/javafx/scene/image/ImageView.html
         Image image = new Image("file:Sprites/map.png", 480*4.5, 320*4.5, false, false);
         ImageView spriteBackGround = new ImageView(image);
-        gamePane.getChildren().add(spriteBackGround);  // Añadir el sprite a la escena
+        // De esta manera consigo contener en un solo panel todo el fondo para manejarlo mas facilmente (Aqui puedo incluir los npc)
+        backgraundPane.getChildren().add(spriteBackGround);
+        gamePane.getChildren().add(backgraundPane);
 
-        // Cargar el sprite sheet (una imagen con múltiples fotogramas en fila) (ancho de la imagen * cuanto quiero que se agrande * cantidad de fotogramas)
-        Image spritePlayerSheet = new Image("file:Sprites/PlayerSheets.png", 16*4*frameCount, 32*4*SheetCount, false, false);
-
-        // Crear un ImageView y solo mostrar el primer fotograma
-        ImageView spritePlayer = new ImageView(spritePlayerSheet);
-        spritePlayer.setX(608);
-        spritePlayer.setY(296);
-        spritePlayer.setViewport(new javafx.geometry.Rectangle2D(0, SheetIndex*frameHeight, frameWidth, frameHeight));
-        gamePane.getChildren().add(spritePlayer);
-
-        // Crear una animación para cambiar los fotogramas cada 200ms
-        Timeline animationPlayer = new Timeline(
-            new KeyFrame(Duration.millis(200), event -> {
-                frameIndex = (frameIndex + 1) % frameCount; // Cambiar el índice del fotograma
-                spritePlayer.setViewport(new javafx.geometry.Rectangle2D(frameIndex * frameWidth, SheetIndex*frameHeight, frameWidth, frameHeight));
-            })
-        );
-        animationPlayer.setCycleCount(Timeline.INDEFINITE); // Repetir indefinidamente
-        animationPlayer.play(); // Hago que la animacion del jugador este siempre activa
+        Player jugador = new Player("file:Sprites/PlayerSheets.png", 16, 32, 4, 4, 4, gamePane);
+        jugador.startAnimation();
+        jugador.setPosition(608, 296);
 
         // Establezco la escena
         Scene scene = new Scene(gamePane, 1280, 720);
@@ -71,7 +53,7 @@ public class Interfaz extends Application {
         Timeline timelineMovment = new Timeline(
             new KeyFrame(Duration.seconds(1.0 / 60), event -> {
                 // Esto hace que se ejecute una vez cada 60 seg
-                moveBackground(spriteBackGround, keysPressed.toString().charAt(1), spritePlayer);
+                moveBackground(backgraundPane, keysPressed.toString().charAt(1), jugador);
             })
         );
         // Esto hace que se ejecute indefinidamente
@@ -79,13 +61,13 @@ public class Interfaz extends Application {
         
         // Manejo de teclas, se ejecuta cuando se presiona una tecla y si es (W, A, S, D) empieza o termina la animación
         scene.setOnKeyPressed(event -> {
-            if(event.getCode() == KeyCode.W || event.getCode() == KeyCode.A || event.getCode() == KeyCode.S || event.getCode() == KeyCode.D) {
+            if("WASD".contains(event.getCode().toString())) {
                 keysPressed.add(event.getCode().toString());
                 timelineMovment.play();
             }
         });
         scene.setOnKeyReleased(event -> {
-            if(event.getCode() == KeyCode.W || event.getCode() == KeyCode.A || event.getCode() == KeyCode.S || event.getCode() == KeyCode.D) {
+            if("WASD".contains(event.getCode().toString())) {
                 keysPressed.remove(event.getCode().toString());
                 timelineMovment.pause();
             }
@@ -113,37 +95,24 @@ public class Interfaz extends Application {
      * @param key La tecla que se ha pulsado, puede ser 'A', 'D', 'W', 'S'.
      * @param spritePlayer El ImageView que representa al jugador.
      */
-    public void moveBackground(ImageView spriteBackGround, char key, ImageView spritePlayer) {
+    public void moveBackground(Pane backgraundPane, char key, Player jugador) {
         switch (key) {
             case 'A':
-                // Si la direcion del sprite no es la correcta la cambio
-                if(SheetIndex != 3){
-                    SheetIndex = 3;
-                    spritePlayer.setViewport(new javafx.geometry.Rectangle2D(frameIndex * frameWidth, SheetIndex*frameHeight, frameWidth, frameHeight));
-                }
-                // Y hago que se mueva el fondo
-                spriteBackGround.setX(spriteBackGround.getX() + 10);
+                if(jugador.getSheetIndex() != 3) jugador.actualizarSheet(3);
+                // Y hago que se mueva el fondo y todo lo que este en backgraundPane como los npc
+                backgraundPane.setTranslateX(backgraundPane.getTranslateX() + velocidad);
                 break;
             case 'D':
-                if(SheetIndex != 1){
-                    SheetIndex = 1;
-                    spritePlayer.setViewport(new javafx.geometry.Rectangle2D(frameIndex * frameWidth, SheetIndex*frameHeight, frameWidth, frameHeight));
-                }
-                spriteBackGround.setX(spriteBackGround.getX() - 10);
+                if(jugador.getSheetIndex() != 1) jugador.actualizarSheet(1);
+                backgraundPane.setTranslateX(backgraundPane.getTranslateX() - velocidad);
                 break;
             case 'W':
-                if(SheetIndex != 2){                  
-                    SheetIndex = 2;
-                    spritePlayer.setViewport(new javafx.geometry.Rectangle2D(frameIndex * frameWidth, SheetIndex*frameHeight, frameWidth, frameHeight));
-                }
-                spriteBackGround.setY(spriteBackGround.getY() + 10);
+                if(jugador.getSheetIndex() != 2) jugador.actualizarSheet(2);
+                backgraundPane.setTranslateY(backgraundPane.getTranslateY() + velocidad);
                 break;
             case 'S':
-                if(SheetIndex != 0){
-                    SheetIndex = 0;
-                    spritePlayer.setViewport(new javafx.geometry.Rectangle2D(frameIndex * frameWidth, SheetIndex*frameHeight, frameWidth, frameHeight));
-                }
-                spriteBackGround.setY(spriteBackGround.getY() - 10);
+                if(jugador.getSheetIndex() != 0) jugador.actualizarSheet(0);
+                backgraundPane.setTranslateY(backgraundPane.getTranslateY() - velocidad);
                 break;
             default:
                 break;
