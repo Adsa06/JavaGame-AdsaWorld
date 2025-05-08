9759702666c4bda35efbf4965bb0c3fc2bbeeb77
Aitor de Santos Amoros <soyadsa06@gmail.com>
2025-05-08 23:35:37 +0200

    Agregacion de un sistema para guardar usuario y contraseña

--- src/main/java/dev/adsa/controlador/LogInMenuController.java
+++ src/main/java/dev/adsa/controlador/LogInMenuController.java
@@ -3,17 +3,28 @@ package dev.adsa.controlador;
 import dev.adsa.utils.GestorPantallas;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
 
 public class LogInMenuController {
 
     @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
     private void iniciarSesion(ActionEvent event) {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+        // Ahora puedes trabajar con username y password
+        System.out.println("Usuario: " + username + ", Contraseña: " + password);
         GestorPantallas.mostrarMenuPrincipal();
     }
 
     @FXML
     private void irARegistrarse(ActionEvent event) {
-        // Lógica para ir a la pantalla de registro
         System.out.println("Ir a registrarse presionado");
     }
 }
